0a4aa6cf59f9d5a43e48879409ccd2ffbd1c1499
Allen <allenshamrock37@gmail.com>
2024-04-14 00:26:37 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -83,16 +83,43 @@ class CheckSession(Resource):
             return user.to_dict(), 200
         
         return {}, 401
+    
+@app.before_request
+def check_if_logged_in():
+    access = [
+        'clear',
+        'article_list',
+        'show_article',
+        'login',
+        'check_session'
+    ]
+
+    if (request.endpoint) not in access and (not session.get('user_id')):
+        return {'error': '401 Unauthorized'}, 401
+
 
 class MemberOnlyIndex(Resource):
-    
+
     def get(self):
-        pass
+        if not session.get('user_id'):
+            return {'message': 'Unauthorized. Please log in to access this content.'}, 401
+
+        member_articles = [article.to_dict() for article in Article.query.filter_by(
+            is_member_only=True).all()]
+        return make_response(jsonify(member_articles), 200)
+
 
 class MemberOnlyArticle(Resource):
-    
+
     def get(self, id):
-        pass
+        if not session.get('user_id'):
+            return {'message': 'Unauthorized. Please log in to access this content.'}, 401
+
+        article = Article.query.filter_by(id=id, is_member_only=True).first()
+        if article:
+            return article.to_dict(), 200
+        else:
+            return {'message': 'Article not found or not accessible.'}, 404
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
